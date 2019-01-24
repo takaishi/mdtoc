f476fc4bad05619f651e791e801ef6ad5979074e
r_takaishi <ryo.takaishi.0@gmail.com>
2019-01-24 12:51:15 +0900

    parse and display headers

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/takaishi/mdtoc
+
+require (
+	github.com/russross/blackfriday v2.0.0+incompatible
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"github.com/russross/blackfriday"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello")
+	input, err := ioutil.ReadFile("./test.md")
+	if err != nil {
+		fmt.Println("failed to read file")
+		os.Exit(-1)
+	}
+	parser := blackfriday.New()
+	node := parser.Parse(input)
+
+	node.Walk(func(node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
+		if node.Type == blackfriday.Heading {
+			anchor := string(node.FirstChild.Literal)
+			anchor = strings.Replace(anchor, " ", "", -1)
+			anchor = strings.Replace(anchor, ".", "", -1)
+
+			fmt.Printf("%s [%s](%s)\n", strings.Repeat("*", node.Level), node.FirstChild.Literal, anchor)
+
+			if node.Next != nil {
+				*node = *node.Next
+			} else {
+				return blackfriday.Terminate
+			}
+		}
+		return blackfriday.GoToNext
+	})
 }
