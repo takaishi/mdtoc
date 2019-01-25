39ac028c2379c72275955f9c76aa50ec8d79eb60
r_takaishi <ryo.takaishi.0@gmail.com>
2019-01-25 15:56:56 +0900

    add test

--- mdtoc/mdtoc_test.go
+++ mdtoc/mdtoc_test.go
@@ -17,6 +17,20 @@ aaa
 bbb
 `
 
+func TestGenerateTOC(t *testing.T) {
+	mt := MDToc{File: "", InFile: false, Level: 2}
+
+	toc := mt.GenerateTOC([]byte(inputTextValid))
+	expect := `
+  * [foo](#foo)
+  * [bar](#bar)`
+
+	if toc != expect {
+		t.Error(toc)
+		t.Error(expect)
+	}
+}
+
 func TestGenerateWithTOC(t *testing.T) {
 	mt := MDToc{File: "", InFile: false, Level: 2}
 
@@ -27,6 +41,35 @@ func TestGenerateWithTOC(t *testing.T) {
 
 	if toc != expect {
 		t.Error(toc)
+		t.Error(expect)
+	}
+
+	output, err := mt.GenerateWithTOC(inputTextValid, toc)
+	if err != nil {
+		t.Error(err)
+	}
+	expect = `# This is example
+
+<!-- toc -->
+<!-- toc:start -->
+
+  * [foo](#foo)
+  * [bar](#bar)
+
+<!-- toc:end -->
+
+## foo
+
+aaa
+
+## bar
+
+bbb
+`
+
+	if output != expect {
+		t.Error("unmatch")
+		t.Error(output)
 		t.Error(expect)
 	}
 }
