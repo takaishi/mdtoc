67c981573f0aa2cf54d280e236948f59df79c32d
r_takaishi <ryo.takaishi.0@gmail.com>
2019-01-28 12:11:41 +0900

    add test

--- mdtoc/mdtoc_test.go
+++ mdtoc/mdtoc_test.go
@@ -17,6 +17,35 @@ aaa
 bbb
 `
 
+var inputTextInvalid1 = `# This is example
+
+## foo
+
+aaa
+
+## bar
+
+bbb
+`
+
+var inputTextInvalid2 = `# This is example
+
+<!-- toc -->
+<!-- toc:start -->
+
+  * [foo](#foo)
+  * [bar](#bar)
+
+
+## foo
+
+aaa
+
+## bar
+
+bbb
+`
+
 func TestGenerateTOC(t *testing.T) {
 	mt := MDToc{File: "", InFile: false, Level: 2}
 
@@ -71,5 +100,31 @@ bbb
 		t.Error("unmatch")
 		t.Error(output)
 		t.Error(expect)
+	}
+}
+
+func TestInsertTOCWithInvalidString(t *testing.T) {
+	mt := MDToc{File: "", InFile: false, Level: 2}
+
+	toc := mt.GenerateTOC([]byte(inputTextValid))
+	expect := `
+  * [foo](#foo)
+  * [bar](#bar)`
+
+	if toc != expect {
+		t.Error(toc)
+		t.Error(expect)
+	}
+
+	_, err := mt.InsertTOC(inputTextInvalid1, toc)
+
+	if err.Error() != "Can not find toc_pos comment `<!-- toc -->`" {
+		t.Error()
+	}
+
+	_, err = mt.InsertTOC(inputTextInvalid2, toc)
+
+	if err.Error() != "Can not find toc end position comment `<!-- toc:end -->`." {
+		t.Error()
 	}
 }
